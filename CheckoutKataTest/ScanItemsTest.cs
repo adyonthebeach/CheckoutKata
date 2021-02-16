31d6bccd4f4187a764487955dd76bfd002df90ce
Adrian Walsh <adyonthebeach@hotmail.co.uk>
2021-02-16 22:25:42 +0000

    Updated Scan Tests

--- CheckoutKataTest/ScanItemsTest.cs
+++ CheckoutKataTest/ScanItemsTest.cs
@@ -6,15 +6,39 @@ namespace CheckoutKataTest
 {
     public class Tests
     {
-
         [Test]
-        public void ScanItemTest()
+        public void ScanItemThatExistsInStockListTest()
         {
             var checkoutFactory = new CheckoutFactory();
             var checkout = checkoutFactory.Create(new StockRepository());
-            checkout.ScanItem("Test");
+            checkout.ScanItem("A99");
 
             Assert.AreEqual(1, checkout.ScannedItems.Count);
+        }
+
+        [Test]
+        public void ScanItemThatDoesNotExistsInStockListTest()
+        {
+            var checkoutFactory = new CheckoutFactory();
+            var checkout = checkoutFactory.Create(new StockRepository());
+            checkout.ScanItem("PARTDOESNTEXIST");
+
+            Assert.AreEqual(0, checkout.ScannedItems.Count);
+        }
+
+        [Test]
+        public void ScanMultipleItemsThatExistsInStockListTest()
+        {
+            var checkoutFactory = new CheckoutFactory();
+            var checkout = checkoutFactory.Create(new StockRepository());
+            checkout.ScanItem("A99");
+            checkout.ScanItem("B15");
+            checkout.ScanItem("C40");
+            checkout.ScanItem("A99");
+            checkout.ScanItem("B15");
+            checkout.ScanItem("C40");
+
+            Assert.AreEqual(6, checkout.ScannedItems.Count);
         }
     }
 }
